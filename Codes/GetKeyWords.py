4c773c64172c8a9c800f0c923851c6f2dd46bc92
XUE Yu, Gavin <75661796+XueYuGavin@users.noreply.github.com>
2021-03-22 17:00:25 +0800

    Update GetKeyWords.py

--- Codes/GetKeyWords.py
+++ Codes/GetKeyWords.py
@@ -11,6 +11,5 @@ for i in lines:
         K1_list=K1_list.strip()
         K1_list=K1_list.split(';')
         keywords.append(K1_list)
-print(keywords)
 p_k=pd.DataFrame(data=keywords)
 p_k.to_csv("/Users/gavin/Library/Mobile Documents/com~apple~CloudDocs/G's/Prostgraduate in CityU_HK/Sememter B/COM5506 Social Network Analysis for Communication/COM5506_Coding& Data/GroupProject/DATA/k_p.csv",encoding='utf-8')
